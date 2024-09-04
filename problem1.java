1d8bace287e50f3b8547ffd58cf7e802d85f5ec9
ttdangg <ttdang1445@gmail.com>
2024-09-04 09:57:25 -0700

    favorite food

--- problem1.java
+++ problem1.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String FavoriteFood = "sushi";
+        System.out.println("I like to eat "+ FavoriteFood + " as well!" );
+
+    }
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,8 @@
 // Create your class here; don't forget the main method
+public class problem2
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
