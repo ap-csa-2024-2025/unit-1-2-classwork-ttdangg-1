52ead13f4476e8b124a424b9a2be7a42ca69f29b
ttdangg <ttdang1445@gmail.com>
2024-09-04 10:04:16 -0700

    added to problem 2 and 3

--- problem2.java
+++ problem2.java
@@ -3,6 +3,10 @@ public class problem2
 {
     public static void main(String[] args)
     {
-        
+        String name1 = "Sandy";
+        String name2 = "David";
+        String name3 = "Elaine";
+        String name4 = "Jesse";
+        System.err.print(name4 + " "+ name2 + " " + name3 + " " + name1);
     }
 }
--- problem3.java
+++ problem3.java
@@ -1,1 +1,10 @@
 // Create your class here; don't forget the main method
+public class problem3
+{
+    public static void main(String[] args)
+    {
+        String name = "Tricia";
+        String state = "California";
+        System.out.println("My name is " + name +"." + "I live in " + state +".");
+    }
+}
